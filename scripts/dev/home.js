32c8f7c803a15a5a2fe2bfe5473284b609ddec7d
nac0n <lager_91@hotmail.com>
2018-01-20 01:02:54 +0100

    added express etc

--- scripts/dev/home.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-	console.log("..och ett skript!")
-})
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static('scripts'))
+
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, "/views/home/index.html"));
 })
